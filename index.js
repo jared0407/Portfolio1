ab42ea99f0d60b3503e1849f47155efd6da6eff5
Jared <38408172+jared0407@users.noreply.github.com>
2021-03-07 18:21:26 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default class Menu extends React.Component {
+    render() {
+        return (
+            <div><p>${this.props.itemPrice}</p>
+                <h2>{this.props.itemName}</h2>
+                <p>{this.props.itemDescription}</p>
+                <p><button>Add to cart</button></p></div>
+                
+        );
+        
+    }
+    
+}
